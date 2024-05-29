ddfee03d985b1a771f4f9d61fdb8d959ccc5a102
K-1022 <kikunagaryu2004@gmail.com>
2024-05-29 23:38:44 +0900

    税込価格表示への変更

--- 応用問題3/script.js
+++ 応用問題3/script.js
@@ -1,9 +1,10 @@
 btn.addEventListener("click", () => {
   const name = document.forms.product.productName.value;
   const price = document.forms.product.productPrice.value;
-  console.log(`${name}は税込み${price}円です。`);
+  let total = price * 1.08;
+  console.log(`${name}は税込み${total}円です。`);
 
   if (price >= 1000) {
-    console.log(`${price}円は高いので買うのをやめます。`);
+    console.log(`${total}円は高いので買うのをやめます。`);
   }
 });
