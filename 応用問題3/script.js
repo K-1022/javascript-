81c98f9c5bbe7b622cb22b3894e3edb6425ea5ff
K-1022 <kikunagaryu2004@gmail.com>
2024-06-05 13:27:55 +0900

    4章課題

--- 応用問題3/script.js
+++ 応用問題3/script.js
@@ -1,10 +1,26 @@
 btn.addEventListener("click", () => {
   const name = document.forms.product.productName.value;
   const price = document.forms.product.productPrice.value;
-  let total = price * 1.08;
-  console.log(`${name}は税込み${total}円です。`);
 
-  if (total >= 1000) {
-    console.log(`${name}は高いので買うのをやめます。`);
+  // バリデーションチェック
+  if (name.trim() === "") {
+    const productNameError = document.getElementById("productNameError");
+    productNameError.innerHTML = "名前を入力してください";
+  } else {
+    productNameError.innerHTML = "";
+  }
+
+  if (price.trim() === "") {
+    const productPriceError = document.getElementById("productPriceError");
+    productPriceError.innerHTML = "価格を入力してください";
+  } else {
+    productPriceError.innerHTML = "";
+
+    let total = price * 1.08;
+    console.log(`${name}は税込み${total}円です。`);
+
+    if (total >= 1000) {
+      console.log(`${name}は高いので買うのをやめます。`);
+    }
   }
 });
