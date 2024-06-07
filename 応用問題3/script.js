b0e1dd4627f38f89fea516555f26dc6c0ba58534
K-1022 <kikunagaryu2004@gmail.com>
2024-06-08 03:49:43 +0900

    else ifを追加しました

--- 応用問題3/script.js
+++ 応用問題3/script.js
@@ -2,25 +2,27 @@ btn.addEventListener("click", () => {
   const name = document.forms.product.productName.value;
   const price = document.forms.product.productPrice.value;
 
-  // バリデーションチェック
+  //バリデーションチェック;
   if (name.trim() === "") {
     const productNameError = document.getElementById("productNameError");
     productNameError.innerHTML = "名前を入力してください";
   } else {
     productNameError.innerHTML = "";
+  }
+  if (price.trim() === "") {
+    const productPriceError = document.getElementById("productPriceError");
+    productPriceError.innerHTML = "価格を入力してください";
+  } else if (name.trim() === "") {
+    const productNameError = document.getElementById("productNameError");
+    productNameError.innerHTML = "名前を入力してください";
+  } else {
+    productPriceError.innerHTML = "";
 
-    if (price.trim() === "") {
-      const productPriceError = document.getElementById("productPriceError");
-      productPriceError.innerHTML = "価格を入力してください";
-    } else {
-      productPriceError.innerHTML = "";
+    let total = price * 1.08;
+    console.log(`${name}は税込み${total}円です。`);
 
-      let total = price * 1.08;
-      console.log(`${name}は税込み${total}円です。`);
-
-      if (total >= 1000) {
-        console.log(`${name}は高いので買うのをやめます。`);
-      }
+    if (total >= 1000) {
+      console.log(`${name}は高いので買うのをやめます。`);
     }
   }
 });
