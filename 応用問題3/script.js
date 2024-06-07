ca746f0a26df8f60fa672a026173cc600b078483
K-1022 <kikunagaryu2004@gmail.com>
2024-06-07 13:17:00 +0900

    }の位置修正

--- 応用問題3/script.js
+++ 応用問題3/script.js
@@ -8,19 +8,19 @@ btn.addEventListener("click", () => {
     productNameError.innerHTML = "名前を入力してください";
   } else {
     productNameError.innerHTML = "";
-  }
 
-  if (price.trim() === "") {
-    const productPriceError = document.getElementById("productPriceError");
-    productPriceError.innerHTML = "価格を入力してください";
-  } else {
-    productPriceError.innerHTML = "";
+    if (price.trim() === "") {
+      const productPriceError = document.getElementById("productPriceError");
+      productPriceError.innerHTML = "価格を入力してください";
+    } else {
+      productPriceError.innerHTML = "";
 
-    let total = price * 1.08;
-    console.log(`${name}は税込み${total}円です。`);
+      let total = price * 1.08;
+      console.log(`${name}は税込み${total}円です。`);
 
-    if (total >= 1000) {
-      console.log(`${name}は高いので買うのをやめます。`);
+      if (total >= 1000) {
+        console.log(`${name}は高いので買うのをやめます。`);
+      }
     }
   }
 });
